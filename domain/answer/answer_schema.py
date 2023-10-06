f7d4fba75b0d07fb55deaf5618a2bd225fe71b03
이명진 <ellenep94@gmail.com>
2023-10-06 10:55:52 +0900

    파이보 프로젝트 최초 커밋

---
+++ domain/answer/answer_schema.py
@@ -0,0 +1,24 @@
+import datetime
+from pydantic import BaseModel, validator
+from domain.user.user_schema import User
+
+class AnswerCreate(BaseModel):
+    content:str
+
+    @validator('content')
+    def not_empty(cls, v):
+        if not v or not v.strip():
+            raise ValueError('빈 값은 허용하지 않습니다.')
+        return v
+
+class Answer(BaseModel):
+    id:int
+    content:str
+    create_date:datetime.datetime
+    user: User or None
+    question_id: int
+    
+
+
+    class Config:
+        orm_mode=True
---
+++ domain/question/question_schema.py
@@ -0,0 +1,37 @@
+import datetime
+
+from pydantic import BaseModel, validator
+from domain.answer.answer_schema import Answer
+from domain.user.user_schema import User
+
+
+class Question(BaseModel):
+    id:int
+    subject: str
+    content:str
+    create_date:datetime.datetime
+    answers: list[Answer]=[]
+    user: User or None
+    
+
+
+
+    class Config:
+        orm_mode=True
+
+class QuestionCreate(BaseModel):
+    subject: str
+    content: str
+
+    @validator('subject', 'content')
+    def not_empty(cls, v):
+        if not v or not v.strip():
+            raise ValueError('빈 값은 허용되지 않습니다.')
+        return v
+
+
+class QuestionList(BaseModel):
+    total: int = 0
+    question_list: list[Question] = []
+
+   
